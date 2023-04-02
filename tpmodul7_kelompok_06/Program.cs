43260b8332454869a1aba3a02a288f327681cd46
Walid Hanif Ataullah <walidhnf12@gmail.com>
2023-04-02 21:32:22 +0700

    first commit

---
+++ tpmodul7_kelompok_06/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
